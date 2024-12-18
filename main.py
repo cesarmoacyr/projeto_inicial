583dccad5d7459479b5244a74f77da01876ad68c
Moacyr Barros <cesar.moacyr@gmail.com>
2024-12-18 17:41:57 -0300

    Salvando o meu arquivo main novamente

---
+++ main.py
@@ -0,0 +1,18 @@
+print("Jornada de dados")
+
+# Variáveis
+idade = 57
+nome = "Moacyr"
+
+# Print
+print(idade)
+
+# Soma
+novaidade = idade + 1
+print(novaidade)
+
+# Lista
+lista = [1,2,3,4,5]
+
+# Métodos
+lista_invertida = lista.reverse()
